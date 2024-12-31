d12a8d2dc86d78f7514ed4a0c6d2e4e74e76720e
bingxueyizu <214866144@qq.com>
2024-12-31 13:37:59 +0800

    auto deploy

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -87,7 +87,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/logo.jpg', // 导航栏logo
-    repo: 'bingxueyizu/blog', // 导航栏右侧生成Github链接
+    repo: 'bingxueyizu/bingxueyizu.github.io', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
     docsDir: 'docs', // 编辑的文件夹
@@ -299,9 +299,9 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         options: {
           clientID: 'a6e1355287947096b88b',
           clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-          repo: 'blog-gitalk-comment', // GitHub 仓库
-          owner: 'xugaoyi', // GitHub仓库所有者
-          admin: ['xugaoyi'], // 对仓库有写权限的人
+          repo: 'bingxueyizu.github.io', // GitHub 仓库
+          owner: 'bingxueyizu', // GitHub仓库所有者
+          admin: ['bingxueyizu'], // 对仓库有写权限的人
           // distractionFreeMode: true,
           pagerDirection: 'last', // 'first'正序 | 'last'倒序
           id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
